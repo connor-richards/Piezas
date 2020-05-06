56d337ec578b25e082f6e146c474bbb7927f7f83
connor-richards <connor.a.t.richards@gmail.com>
2020-05-06 13:39:04 -0700

    a test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -1,20 +1,31 @@
 /**
  * Unit Tests for Piezas
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
-	protected:
-		PiezasTest(){} //constructor runs before each test
-		virtual ~PiezasTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+  protected:
+    PiezasTest(){} //constructor runs before each test
+    virtual ~PiezasTest(){} //destructor cleans up after tests
+    virtual void SetUp(){} //sets up before each test (after constructor)
+    virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
 TEST(PiezasTest, sanityCheck)
 {
-	ASSERT_TRUE(true);
+  ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, sanityCheck)
+{
+  Piezas game;
+  if(game.gameState() == Invalid) {
+    ASSERT_TRUE(true);
+  }
+  else {
+    ASSERT_TRUE(false);
+  }
 }
